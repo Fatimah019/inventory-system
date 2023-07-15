04c0ae487172e323d512f2efddaf6dd2320ebf3d
Soumyajit <mondalsoumya02@gmail.com>
2023-07-15 11:39:31 +0530

    Change in the home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,15 +1,54 @@
 import React from "react";
+import inventoryManagementImage from "./inventory-management.png";
+import image1 from './images.png';
+import image2 from './Custom-inventory-management-app-with-kizeo-forms-01.jpg';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to our website</h1>
-      <p>Discover the world of inventory management with us!</p>
-      {/* Add your pictures here */}
-      {/* <img src="image1.jpg" alt="Image 1" />
-      <img src="image2.jpg" alt="Image 2" /> */}
+    <div style={containerStyles}>
+      <div style={textContainerStyles}>
+        <h1 style={textStyles}>Welcome to our website</h1>
+        <p style={textStyles}>Discover the world of inventory management with us!</p>
+      </div>
+
+      <div style={imageContainerStyles}>
+        <img src={inventoryManagementImage} alt="Inventory Management" style={imageStyles} />
+        <img src={image1} alt="Image 1" style={imageStyles} />
+        <img src={image2} alt="Image 2" style={imageStyles} />
+      </div>
     </div>
   );
 }
+const containerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  // height: "100vh",
+  overflow: "hidden", 
+};
+
+const textContainerStyles = {
+  textAlign: "center",
+  marginBottom: "2rem",
+  animation: "slide-in-left 1s ease",
+};
+
+const textStyles = {
+  margin: 0,
+};
+
+const imageContainerStyles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+const imageStyles = {
+  width: "300px",
+  height: "300px",
+  margin: "0 10px",
+  border: "2px solid white",
+};
 
 export default Home;
